c2a855a1fe78aa247c2be754f5eb23be6e2815c5
Mark McCarthy <mark.joseph.mccarthy@gmail.com>
2023-07-13 13:45:47 +0100

    first commit

---
+++ lists.py
@@ -0,0 +1,15 @@
+my_fav_songs = ["Zoom - Fat Larrys Band", "American Pie - Don McLean", "Bohemian Rhapsody - Queen"]
+
+print(my_fav_songs)
+
+my_fav_songs.append("The fields of Athenry")
+
+print(my_fav_songs)
+
+# my_fav_songs.pop()
+# my_fav_songs.pop()
+
+# Example of .remove() method
+my_fav_songs.remove("Zoom - Fat Larrys Band")
+
+print(my_fav_songs)
---
+++ recap.py
@@ -0,0 +1,10 @@
+def multiple(number1,number2):
+    return number1 * number2
+
+print(multiple(5,7))
+
+coffee_order = [
+    "Chris - Bica", "Gezer - Galao", "Andrew - cafe com leite", "Mark - Cha"
+]
+
+print(coffee_order)
